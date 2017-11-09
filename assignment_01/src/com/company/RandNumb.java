d13fbc9f45f9856da14f0198635add4191a773d8
leonguyen020 <leonguyen020@gmail.com>
2017-11-09 11:47:02 +0700

    Add existing file

--- assignment_01/src/com/company/RandNumb.java
+++ assignment_01/src/com/company/RandNumb.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 public class RandNumb {
     protected static int randNumb(){
-        Random rand = new Random(); // Declare random variable
-        return rand.nextInt(46);
+        
     }
 }
