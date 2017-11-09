90f3c36f23f4b2dc67f30ef430bc912a5417ee4f
leonguyen020 <leonguyen020@gmail.com>
2017-11-09 23:26:45 +0700

    Not yet finish EditTicket

--- assignment_01 - Advanced/src/com/company/EditTicket.java
+++ assignment_01 - Advanced/src/com/company/EditTicket.java
@@ -1,29 +1,38 @@
 package com.company;
 
+import com.sun.tools.jdeprscan.scan.Scan;
+
 import java.util.Scanner;
 
 public class EditTicket {
-    protected static void setValue(int[] array, int val){
+    protected static void setValue(int[] array){
         boolean Error;
+
+        // position checking
         int pos_int = 0;
         Scanner sc_pos = new Scanner(System.in);
         do {
             Error = false; // Set boolean error
+            System.out.print("Which position of your ticket you want to change? ");
             String pos = sc_pos.next(); // Position of edited number
             try{
                 pos_int = Integer.parseInt(pos);
                 if(pos_int < 1 || pos_int > 6){
-                    System.out.println("\033[31;1mIt is not a correct position ticket number. Please enter again\033[0m");
+                    System.out.println( "\033[31;1mIt is not a correct position ticket number. " +
+                                        "Please enter again.\033[0m");
                     Error = true;
                 }
-                else Error = false;
+//                else Error = false;
             }catch (NumberFormatException e){
-                System.out.println("Invalid Input. Please enter again.");
+                System.out.println("\033[31;1mInvalid Input. Please enter again.\033[0m");
                 Error = true;
             }
 
         }while(Error);
 
+        // Value checking
+        int val_int = 0;
+        Scanner sc_val = new Scanner(System.in);
 
     }
 }
