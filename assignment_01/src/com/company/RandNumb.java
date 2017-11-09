8ad4f5ab02ab80e62abe221edd0043dba8a475ad
leonguyen020 <leonguyen020@gmail.com>
2017-11-09 10:46:15 +0700

    Initial Commit

--- assignment_01/src/com/company/RandNumb.java
+++ assignment_01/src/com/company/RandNumb.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.Random;
 
+// New change to check git
+
 public class RandNumb {
     protected static int randNumb(){
         Random rand = new Random(); // Declare random variable
