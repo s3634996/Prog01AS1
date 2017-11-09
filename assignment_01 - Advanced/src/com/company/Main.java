0b56af67a93d904586bdf7d7f97240aa6defbe78
leonguyen020 <leonguyen020@gmail.com>
2017-11-09 22:02:29 +0700

    Update Existing File

--- assignment_01 - Advanced/src/com/company/Main.java
+++ assignment_01 - Advanced/src/com/company/Main.java
@@ -43,6 +43,22 @@ public class Main {
 
         System.out.println("You are eligible to do the lottery. You will begin to choose number shortly.");
 
+        boolean retype;
+        do{
+            retype = true;
+            System.out.print("Do you want to buy new ticket? (Y or N) ");
+            Scanner re = new Scanner(System.in);
+            String user_input = re.next();
+            if(user_input.equalsIgnoreCase("y")){
+                retype = true;
+            }else if(user_input.equalsIgnoreCase("n")){
+                System.out.println("Thank you for buying our ticket. Best luck to you next time.");
+                System.exit(0);
+            }else{
+                System.out.println("Invalid Input. Please enter again.");
+                retype = false;
+            }
+        }while(retype = false);
 
     }
 
