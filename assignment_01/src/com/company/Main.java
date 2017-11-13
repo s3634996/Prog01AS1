ab4e1e3b3541629e4bec929411fb9238e9e07cc6
leonguyen020 <leonguyen020@gmail.com>
2017-11-13 22:53:08 +0700

    Add retry.java in assignment_01

--- assignment_01/src/com/company/Main.java
+++ assignment_01/src/com/company/Main.java
@@ -15,60 +15,44 @@ public class Main {
 //        System.out.println("You are eligible to do the lottery. You will begin to choose number shortly.");
 
         int buying_ticket = 1;
-
-
-        // Customer ticket
-        System.out.println("This will be your "+OrdinalConvert.convert(buying_ticket-1)+" ticket for today.");
-        int[] ticket = Ticket.ticket();
-
-
-        // Display ticket
-        System.out.println("This is your final ticket number");
-        for (int i = 0; i < ticket.length; i++) {
-            System.out.print(ticket[i]+" ");
-            Thread.sleep(500);
-        }
-        System.out.println();
-        Arrays.sort(ticket);
-
-
-        // Lottery number
-        System.out.println("This is our number for today.");
-        int[] lottery = Lottery.lottery();
-        System.out.println();
-        Arrays.sort(lottery);
-
-
-        // Comparing
-        if(Arrays.equals(lottery, ticket))
-            System.out.println("You are the winner.");
-        else
-            System.out.println("You do not win this time. Please try again.");
-
-
-        // Whether user wants to buy ticket
-        boolean retype;
+        boolean retry;
         do{
-            retype = false;
-            System.out.print("Do you want to buy new ticket? (Y or N) ");
-            Scanner re = new Scanner(System.in);
-            String user_input = re.next();
-            if(user_input.equalsIgnoreCase("y")){
-                buying_ticket += 1;
-                retype = false;
-            }
-            else if(user_input.equalsIgnoreCase("n")){
-                System.out.println("Thank you for buying our ticket. Best luck to you next time.");
-                System.exit(0);
-            }
-            else{
-                System.out.println("Invalid Input. Please enter again.");
-                retype = true;
-            }
-        }while(retype);
-        // Enter Y will make retype = false and exit the loop
-        // but need to reset the game
+            retry = true;
+            // Customer ticket
+            System.out.println("This will be your "+OrdinalConvert.convert(buying_ticket-1)+" ticket for today.");
+            int[] ticket = Ticket.ticket();
 
+
+            // Display ticket
+            System.out.println("This is your final ticket number");
+            for (int i = 0; i < ticket.length; i++) {
+                System.out.print(ticket[i]+" ");
+                Thread.sleep(500);
+            }
+            System.out.println();
+            Arrays.sort(ticket);
+
+
+            // Lottery number
+            System.out.println("This is our number for today.");
+            int[] lottery = Lottery.lottery();
+            System.out.println();
+            Arrays.sort(lottery);
+
+
+            // Comparing
+            if(Arrays.equals(lottery, ticket))
+                System.out.println("You are the winner.");
+            else
+                System.out.println("You do not win this time. Please try again.");
+
+
+            // Whether user wants to buy ticket
+            buying_ticket = Retry.ticket_ascend(buying_ticket);
+            // Enter Y will make retype = false and exit the loop
+            // but need to reset the game
+
+        }while (retry);
 
     }
 
--- assignment_01/src/com/company/Retry.java
+++ assignment_01/src/com/company/Retry.java
@@ -1,5 +1,29 @@
 package com.company;
 
-public class Retry {
+import java.util.Scanner;
 
+public class Retry {
+    protected static int ticket_ascend(int buying_ticket){
+        boolean retype;
+        do{
+            retype = false;
+            System.out.print("Do you want to buy new ticket? (Y or N) ");
+            Scanner re = new Scanner(System.in);
+            String user_input = re.next();
+            if(user_input.equalsIgnoreCase("y")){
+                buying_ticket += 1;
+                retype = false;
+            }
+            else if(user_input.equalsIgnoreCase("n")){
+                System.out.println("Thank you for buying our ticket. Best luck to you next time.");
+                System.exit(0);
+            }
+            else{
+                System.out.println("Invalid Input. Please enter again.");
+                retype = true;
+            }
+        }while(retype);
+
+        return buying_ticket;
+    }
 }
